6110d2c0657e0b30e2e605a5c9eb04a9228363cc
subhojit paul <subhojitpaul26042004@gmail.com>
2026-09-10 22:12:19 +0530

    chore: bump version to 2.4.0 (versionCode 117)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
     applicationId = "com.subhojit.lksdialer.app"
     minSdk = 24
     targetSdk = 35
-    versionCode = 116
-    versionName = "2.3.0"
+    versionCode = 117
+    versionName = "2.4.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
