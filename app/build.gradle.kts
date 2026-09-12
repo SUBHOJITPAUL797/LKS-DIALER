bc5958c40b03c2fbdbed26f2f8c10e067ff37173
subhojit paul <subhojitpaul26042004@gmail.com>
2026-09-12 21:27:39 +0530

    chore: bump version to 2.6.4 (132)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
     applicationId = "com.subhojit.lksdialer.app"
     minSdk = 24
     targetSdk = 35
-    versionCode = 131
-    versionName = "2.6.3"
+    versionCode = 132
+    versionName = "2.6.4"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
